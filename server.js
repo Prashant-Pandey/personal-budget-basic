11cc6eb3b5dea4761a01b4eb69ff3acf7fe03fa6
Prashant Pandey <prashant.pandey.dev@gmail.com>
2020-09-30 22:15:49 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require("cors")
 const budget = require("./budget-data.json")
 const app = express();
 const port = 3000;
+
+
+app.use(cors())
 
 
 app.use('/', express.static('./public'))
@@ -11,6 +15,10 @@ app.get('/hello', (req, res) => {
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
+
+app.get('/budgetFile', (req, res)=>{
+    res.sendFile(__dirname+"/budget-data.json");
+})
 app.get('/get_error', (req, res) => {
     res.status(400);
     res.send({ error: 'file not found' })
